577829b4471f3c963e56428dc15b1d35e77a62ce
ted <teddsbennett@gmail.com>
2020-02-02 16:12:51 +0000

    copy over socket and client from battleships

---
+++ client.py
@@ -0,0 +1,44 @@
+# !/usr/bin/env python3
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
+
+
+class Client:
+    def __init__(self, board):
+        self.host = "192.168.68.120"
+        self.port = 33000
+        self.buffer_size = 1024
+
+        self.name = None
+        self.client_socket = None
+        self.board = board
+
+        self.start_client()
+
+    def start_client(self):
+        self.client_socket = socket(AF_INET, SOCK_STREAM)
+        self.client_socket.connect((self.host, self.port))
+
+        receive_thread = Thread(target=self.receive)
+        receive_thread.start()
+
+    def receive(self):
+        """Handles receiving of messages."""
+        while True:
+            try:
+                message = self.client_socket.recv(self.buffer_size).decode("utf8")
+                message = message.split(',')
+                print("received {}".format(message))
+                response = self.board.process_message(message)
+                if response:
+                    self.send(response)
+            except OSError:
+                break
+
+    def send(self, message: str):
+        """Handles sending of messages."""
+        print("sent {}".format(message))
+        self.client_socket.send(bytes(message, "utf8"))
+
+    def close(self):
+        self.send("EXIT")
---
+++ server.py
@@ -0,0 +1,62 @@
+# !/usr/bin/env python3
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
+
+
+class Server:
+    def __init__(self):
+        self.clients = {}
+        self.addresses = {}
+        self.start_server()
+
+    def start_server(self):
+        SERVER.listen(5)
+        print("Waiting for connection...")
+        accept_thread = Thread(target=self.accept_incoming_connections)
+        accept_thread.start()
+        accept_thread.join()
+        SERVER.close()
+
+    def accept_incoming_connections(self):
+        """Sets up handling for incoming clients."""
+        num_clients = 0
+        while True:
+            client, client_address = SERVER.accept()
+            self.addresses[client] = client_address
+            print("%s:%s has connected." % client_address)
+            Thread(target=self.handle_client, args=(client,)).start()
+
+    def handle_client(self, client):
+        """Handles a single client connection."""
+        name = str(len(self.clients) + 1)
+        self.clients[client] = name
+        self.broadcast(name, bytes("JOIN", "utf8"))
+
+        while True:
+            msg = client.recv(BUFSIZ)
+            if msg != bytes("EXIT", "utf8"):
+                self.broadcast(name, msg)
+            else:
+                client.close()
+                del self.clients[client]
+                print("%s:%s has disconnected." % self.addresses[client])
+                self.broadcast(name, bytes("EXIT", "utf8"))
+                del self.addresses[client]
+                break
+
+    def broadcast(self, name, msg):
+        """Broadcasts a message to all the clients."""
+        for sock in self.clients:
+            sock.send(bytes(name + ",", "utf8") + msg)
+
+
+if __name__ == "__main__":
+    HOST = ''
+    PORT = 33000
+    BUFSIZ = 1024
+    ADDR = (HOST, PORT)
+
+    SERVER = socket(AF_INET, SOCK_STREAM)
+    SERVER.bind(ADDR)
+
+    server = Server()
